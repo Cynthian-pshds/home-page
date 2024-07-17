827b67e4c6c4a1e722fbad35f0a2e71cd289515f
Lyan Ketin Kanon Runningwind Roffi Roffe <roffi.peng@foxmail.com>
2024-07-17 16:28:10 +0800

    放弃

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,14 @@
 import { defineConfig } from 'astro/config';
-import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-import {unified} from "unified";
-import remarkToc from 'remark-toc';
-import {toc} from "@jsdevtools/rehype-toc";
+
+
 
 
 // https://astro.build/config
 
-
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkToc, [remarkToc,{ heading: 'toc', maxDepth: 6 }]],
-    rehypePlugins: [
-      rehypeHeadingIds,
-    ],
-  },
+  markdown: {},
 });
 
 
 
 // Use the Rehype TOC plugin with its default options
-unified().use(toc);
-
-// Use the Rehype TOC plugin with custom options
-unified().use(toc, {
-  headings: ["h1", "h2"],     // Only include <h1> and <h2> headings in the TOC
-  cssClasses: {
-    toc: "page-outline",      // Change the CSS class for the TOC
-    link: "page-link",        // Change the CSS class for links in the TOC
-  }
-});
