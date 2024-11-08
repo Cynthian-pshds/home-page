90641a3611910883eef855118986fa18fe4e6b90
Lyan Ketin Kanon Runningwind Roffi Roffe <x@pengxianzhe.org>
2024-11-08 16:35:07 +0800

    i

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 import { defineConfig } from 'astro/config';
+import { remark } from 'remark';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import { remarkAlert } from 'remark-github-blockquote-alert'
 
+let markdown = `# Alert \n> [!NOTE] \n> test`;
+const htmlStr = remark()
+    .use(remarkParse)
+    .use(remarkAlert)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .processSync(markdown).toString()
 
 
 
 // https://astro.build/config
 
+
 export default defineConfig({
-  markdown: {},
+  markdown: {
+    remarkPlugins: [
+    ],
+  },
 });
 
 
